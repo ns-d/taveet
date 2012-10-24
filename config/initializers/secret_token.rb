87b8d272e34f9887cf9581479be8fabc33f91892
Lucky <lucky0347@yahoo.com>
2012-10-23 20:56:41 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaveetCom::Application.config.secret_token = '1428ae0467f0e627dcd95b3629a4694f3b298643a275bb169bd90f501db8a1f32e087b91ec284f131fb5e25cbc828c20ce4701b79c8e6a1a4f8efd8fff840b0a'
